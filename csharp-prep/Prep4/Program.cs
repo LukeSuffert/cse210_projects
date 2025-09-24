8127d7a34193ed116b89feffa40790ab83844c9e
lukesuffert <lukesuffert@byui.edu>
2025-09-24 09:56:40 -0600

    prep 4 core requi

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        while (number != 0)
+        {
+            numbers.Add(number);
+            Console.Write("Enter a number:");
+            number = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("The sum of all numbers is: " + numbers.Sum());
+        Console.WriteLine("The average of all numbers is: " + numbers.Average());
+        Console.WriteLine("The largest number is: " + numbers.Max());
+
     }
 }
