ecf4a384ac628ad00d909a00eb226a57183130b1
lukesuffert <lukesuffert@byui.edu>
2025-09-24 09:44:29 -0600

    prep 3 core requirements

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random magicNumber = new Random();
+        int number = magicNumber.Next(1, 100);
+
+        Console.WriteLine("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
+        while (guess != number)
+        {
+            if (guess < number)
+            {
+                Console.WriteLine("Higher");
+                Console.WriteLine("What is your guess?");
+                guess = int.Parse(Console.ReadLine());
+            }
+
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+                Console.WriteLine("What is your guess?");
+                guess = int.Parse(Console.ReadLine());
+            }
+        }
+
+                Console.WriteLine("You guessed it!");
+
     }
 }
