b949749c679e129bfb8669bc46eed2e31c43f5fa
lukesuffert <lukesuffert@byui.edu>
2025-10-01 12:59:00 -0600

    ponder W03

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,63 @@
 using System;
 
+public class Job
+{
+    public string _jobTitle;
+    public string _company;
+    public int _startYear;
+    public int _endYear;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
+
+public class Resume
+{
+    public string _name;
+
+    // Make sure to initialize your list to a new List before you use it.
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        // Notice the use of the custom data type "Job" in this loop
+        foreach (Job job in _jobs)
+        {
+            // This calls the Display method on each job
+            job.Display();
+        }
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        
+
+        Job job1 = new Job();
+        job1._jobTitle = "Caterer";
+        job1._company = "Food Services";
+        job1._startYear = 2024;
+        job1._endYear = 2025;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Intern";
+        job2._company = "Microsoft";
+        job2._startYear = 2026;
+        job2._endYear = 2026;
+
+        Resume myResume = new Resume();
+        myResume._name = "Luke Suffert";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
